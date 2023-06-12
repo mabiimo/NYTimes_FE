d52cf72f82dd6a4e0247ceb308ffabbe811208fc
3IA03_ Muhammad Aryo Bimo <muharyo1995@gmail.com>
2023-06-12 14:59:27 +0700

    Update 12 jUne

--- src/Components/Homepage/Carousel.jsx
+++ src/Components/Homepage/Carousel.jsx
@@ -15,18 +15,18 @@ export default function Example() {
   useEffect(() => {
     const getArticles = async () => {
       const data = await fetchArticles();
-      setArticles(data.slice(0, 5)); // Hanya mengambil 5 artikel pertama
+      setArticles(data.slice(0, 5));
     };
     getArticles();
   }, []);
 
   return (
-    <Carousel className="w-[800px] mx-auto rounded-xl mb-10" onInvalid={false}>
+    <Carousel className="w-full sm:w-[600px] md:w-[800px] mx-auto rounded-xl mb-10" onInvalid={false}>
       {articles.map((article, i) => (
         <Link key={i} to="/detailNews" state={{ article }} className="cursor-pointer" onClick={() => navigateToDetail()}>
           <div key={i} className="relative">
             <img src={article.multimedia?.[0]?.url} alt={article.title} />
-            <p className="absolute bottom-0 left-0 w-full p-2 bg-gradient-to-b from-transparent to-black text-white font-bold text-[40px] pb-10">{article.title}</p>
+            <p className="absolute bottom-0 left-0 w-full p-2 bg-gradient-to-b from-transparent to-black text-white font-bold text-lg sm:text-2xl md:text-3xl pb-10">{article.title}</p>
           </div>
         </Link>
       ))}
